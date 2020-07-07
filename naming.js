88489ecc9d815cc127baa6a46baff343d6bf2a06
unknown <william.henke@computingforall.org>
2020-07-07 14:57:15 -0700

    This is the more proper way to do it

--- naming.js
+++ naming.js
@@ -1,4 +1,4 @@
-// These variables are used to amke the page functional.
+// These variables are used to make the page functional.
 var hideButtons = "bananas";
 var showButtons = "oranges";
 var getFirstImage = "coffee";
@@ -12,10 +12,14 @@ const usersAllowed = 500;
 const bacon = 9001;
 const purple = 0.2345;
 
-console.log('This hides buttons when applicaple', hideButtons);
-console.log('This makes the buttons appear when applicable.', showButtons);
-console.log('This gets the first image for use on the page.', getFirstImage);
-console.log('This moves the user to the next page.', moveToNextPage);
-console.log('This changes some colors on the page.', changeColor);
+console.log('hideButtons', hideButtons);
+console.log('showButtons', showButtons);
+console.log('getFirstImage', getFirstImage);
+console.log('moveToNextPage', moveToNextPage);
+console.log('changeColor', changeColor);
 
-console.log('These are the constants:', pi, limit, usersAllowed, bacon, purple);
+console.log('pi', pi);
+console.log('limit', limit);
+console.log('usersAllowed', usersAllowed);
+console.log('bacon', bacon);
+console.log('purple', purple);
