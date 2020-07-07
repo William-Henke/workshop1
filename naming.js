4c1153027151251ff0d41470c367b7a133e1a2c7
unknown <william.henke@computingforall.org>
2020-07-07 14:00:24 -0700

    here are 5 well named variables and 5 constants in a js file

---
+++ naming.js
@@ -0,0 +1,14 @@
+var hideButtons = "bananas";
+var showButtons = "oranges";
+var getImages = "coffee";
+var moveToNextPage = "milk";
+var changeColor = "boo";
+
+const pi = 3.14;
+const limit = 150;
+const usersAllowed = 500;
+const bacon = 9001;
+const purple = 0.2345;
+
+console.log(hideButtons, showButtons, getImages, moveToNextPage, changeColor);
+console.log(pi, limit, usersAllowed, bacon, purple);
