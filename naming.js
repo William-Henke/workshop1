b1d41d898554c7e7862df7d52e26bff0dabf0026
unknown <william.henke@computingforall.org>
2020-07-07 14:40:42 -0700

    This is a more descriptive version than before

--- naming.js
+++ naming.js
@@ -1,14 +1,21 @@
+// These variables are used to amke the page functional.
 var hideButtons = "bananas";
 var showButtons = "oranges";
-var getImages = "coffee";
+var getFirstImage = "coffee";
 var moveToNextPage = "milk";
 var changeColor = "boo";
 
+// These are the constants.
 const pi = 3.14;
 const limit = 150;
 const usersAllowed = 500;
 const bacon = 9001;
 const purple = 0.2345;
 
-console.log(hideButtons, showButtons, getImages, moveToNextPage, changeColor);
-console.log(pi, limit, usersAllowed, bacon, purple);
+console.log('This hides buttons when applicaple', hideButtons);
+console.log('This makes the buttons appear when applicable', showButtons);
+console.log('This gets the first image for use on the page', getFirstImage);
+console.log('This moves the user to the next page', moveToNextPage);
+console.log('This changes some colors on the page', changeColor);
+
+console.log('These are the constants', pi, limit, usersAllowed, bacon, purple);
